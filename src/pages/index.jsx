ed959baad32eb8f8e6dabde71af7af615f0726f5
camille villard <hello@camillevillard.com>
2019-05-17 09:43:31 -0400

    layout fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import { graphql } from "gatsby";
 
 // internal components
-import SEO from "../components/seo";
-import Icons from "../components/icons"
-import MailChimpForm from "../components/mailchimp-form";
+import SEO from "../components/seo.jsx";
+import Icons from "../components/icons.jsx"
+import MailChimpForm from "../components/mailchimp-form.jsx";
 
 // images & assets
 import Logo from "../images/platine_logo-principal.svg";
